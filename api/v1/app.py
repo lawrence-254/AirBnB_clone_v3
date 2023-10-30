a9a0d72ded458920eaa08aca040dd6882f96b186
lawrence-254 <lodhiambo262gmail.com>
2023-10-30 02:33:06 -0700

    index

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,17 +3,27 @@
 Declare a method to handle @app.teardown_appcontext that calls storage.close
 """
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
+from flask_cors import CORS
+from os import getenv
 
 
 app = Flask(__main__)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
 def teardown(exception):
+    """ Closes the DB """
     storage.close()
+
+@app.errorhandler(404)
+def page_not_found(exception):
+    """ Returns a JSON formatted 404 status code response """
+    return jsonify({'error': 'Not found'}), 404
 
 
 if __name__ == "__main__":
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,15 @@
 """Returns a JSON status OK"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def get_status():
+    """Returns status"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """Displslays object and their  aand therproperties""""
+
