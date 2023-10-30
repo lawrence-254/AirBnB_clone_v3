164137551644ce556b1bde873da1a3c9ee55da22
lawrence-254 <lodhiambo262gmail.com>
2023-10-30 03:00:39 -0700

    pep8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,10 +10,17 @@ def get_status():
     """Returns status"""
     return jsonify({"status": "OK"})
 
+
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
     """Displslays object and their properties"""
-    class_dict = {"Amenity": "amenities", "City": "cities", "Place": "places",
-            "Review": "reviews", "State": "states", "User": "users"}
+    class_dict = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+            }
     obj_result = {class_dict[cls]: storage.count(cls) for cls in class_dict}
     return jsonify(obj_result)
