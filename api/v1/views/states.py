6abfed58149ced39898864ab196179b86bd623d9
lawrence-254 <lodhiambo262gmail.com>
2023-10-30 06:47:25 -0700

    updates

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -49,3 +49,34 @@ def delete_state_by_id(state_id=None):
     state.delete()
     storage.save()
     return jsonify({})
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create_state():
+    """Creates a new State."""
+    if request.json is None:
+        return "Not a JSON", 400
+    fields = request.get_json()
+    if fields.get('name') is None:
+        return "Missing name", 400
+    new_state = State(**fields)
+    new_state.save()
+    return jsonify(new_state.to_dict()), 201
+
+
+@app_views.route('/states/<string:state_id>', methods=['PUT'],
+                 strict_slashes=False)
+def update_state(state_id=None):
+    """Updates a State object by ID."""
+    if request.json is None:
+        return "Not a JSON", 400
+    fields = request.get_json()
+    state_obj = storage.get("State", state_id)
+    if state_obj is None:
+        abort(404)
+    for key in fields:
+        if key not in ['id', 'created_at', 'updated_at']:
+            if hasattr(state_obj, key):
+                setattr(state_obj, key, fields[key])
+    state_obj.save()
+    return jsonify(state_obj.to_dict()), 200
